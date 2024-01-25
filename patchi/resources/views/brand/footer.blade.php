000bf63304d5fd16378042ceaaf236e1f004dd4b
deeb <turkdeeb@gmail.com>
2024-01-25 11:41:05 +0200

    updating footer and adding it to login

--- patchi/resources/views/brand/footer.blade.php
+++ patchi/resources/views/brand/footer.blade.php
@@ -1,5 +1,5 @@
 <p class="small m-n">
     <a href="{{ config('app.url') }}" target="_blank">
-        Copyright © Patchi Saudi Arabia, All Rights Reserved ® 2024 {{date('Y')}}
+        Copyright &copy;  Patchi Saudi Arabia, All Rights Reserved 	&reg; 2024 {{date('Y')}}
     </a>
 </p>
